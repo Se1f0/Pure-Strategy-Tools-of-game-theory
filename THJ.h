c1d217339a910e952a008aee4d953b664b5e6985
Segueni Seif Eddine <57069167+Se1f0@users.noreply.github.com>
2021-06-21 00:13:46 +0100

    Add files via upload

--- THJ.h
+++ THJ.h
@@ -9,13 +9,13 @@
 
 using namespace std;
 
-class Strategie{
+class Profile{
     private:
         string name;
         int scorePlayer1;
         int scorePlayer2;
     public:
-        Strategie(string name,int scorePlayer1,int scorePlayer2){
+        Profile(string name,int scorePlayer1,int scorePlayer2){
             this->name = name;
             this->scorePlayer1 = scorePlayer1;
             this->scorePlayer2 = scorePlayer2;
@@ -40,10 +40,10 @@ class Strategie{
         }
 };
 
-vector<vector<Strategie>> matJeu;
+vector<vector<Profile>> matJeu;
 int nb_strategie1,nb_strategie2;
 vector<string>* strategies = new vector<string>();
-vector<vector<Strategie>> v;
+vector<vector<Profile>> v;
 bool lineDel = true,colDel = true;
 
 int determineMDS(int player){
@@ -186,7 +186,7 @@ int determineMDF(int player){
     return -1;
 }
 
-void delLine(){
+void delLine(int type){
     vector<int> temp;
     bool b = true;
     for (int i = 0; i < nb_strategie1; i++)
@@ -198,7 +198,7 @@ void delLine(){
                 b = true;
                 for (int k = 0; k < nb_strategie2; k++)
                 {
-                    if (matJeu[i][k].getScorePlayer1() > matJeu[j][k].getScorePlayer1())
+                    if ((type == 1 && matJeu[i][k].getScorePlayer1() > matJeu[j][k].getScorePlayer1()) || (type == 2 && matJeu[i][k].getScorePlayer1() >= matJeu[j][k].getScorePlayer1()))
                     {
                         b = false;
                         break;
@@ -206,7 +206,14 @@ void delLine(){
                 }
                 if (b)
                 {
-                    cout << "Strategie " << strategies->at(i) <<" for player 1 is dominated by strategie " << strategies->at(j) <<endl;
+                    if (type == 1)
+                    {
+                        cout << "Strategie " << strategies->at(i) <<" for player 1 is dominated by strategie " << strategies->at(j) <<endl;
+                    }
+                    else
+                    {
+                        cout << "Strategie " << strategies->at(i) <<" for player 1 is strictly dominated by strategie " << strategies->at(j) <<endl;
+                    }
                     temp.push_back(i);
                     goto NEXT;
                 }
@@ -216,11 +223,11 @@ void delLine(){
     NEXT:
     if (temp.empty())
     {
-        cout << "There is no  dominant strategy for player 1"<<endl;
+        cout << "There is no strategy that is dominated by another strategy for player 1"<<endl;
     }
     lineDel = !temp.empty();
     vector<int>::iterator it;
-    vector<Strategie> s;
+    vector<Profile> s;
     for (int i = 0; i < nb_strategie1; i++)
     {
         it = find(temp.begin(),temp.end(),i);
@@ -267,7 +274,7 @@ void delLine(){
     v.clear();
 }
 
-void delCol(){
+void delCol(int type){
     vector<int> temp;
     bool b = true;
     for (int i = 0; i < nb_strategie2; i++)
@@ -279,7 +286,7 @@ void delCol(){
                 b = true;
                 for (int k = 0; k < nb_strategie1; k++)
                 {
-                    if (matJeu[k][i].getscorePlayer2() > matJeu[k][j].getscorePlayer2())
+                    if ((type == 1 && matJeu[k][i].getscorePlayer2() > matJeu[k][j].getscorePlayer2()) || (type == 2 && matJeu[k][i].getscorePlayer2() >= matJeu[k][j].getscorePlayer2()))
                     {
                         b = false;
                         break;
@@ -287,7 +294,14 @@ void delCol(){
                 }
                 if (b)
                 {
-                    cout << "Strategie " << strategies->at(i+nb_strategie1) <<" for player 2 is dominated by strategie " << strategies->at(j+nb_strategie1) <<endl;
+                    if (type == 1)
+                    {
+                        cout << "Strategie " << strategies->at(i+nb_strategie1) <<" for player 2 is dominated by strategie " << strategies->at(j+nb_strategie1) <<endl;       
+                    }
+                    else
+                    {
+                        cout << "Strategie " << strategies->at(i+nb_strategie1) <<" for player 2 is strictly dominated by strategie " << strategies->at(j+nb_strategie1) <<endl;
+                    }
                     temp.push_back(i);
                     goto NEXT2;
                 }
@@ -297,11 +311,11 @@ void delCol(){
     NEXT2:
     if (temp.empty())
     {
-        cout << "There is no dominant strategy for player 2"<<endl;
+        cout << "There is no strategy that is dominated by another strategy for player 2"<<endl;
     }
     colDel = !temp.empty();
     vector<int>::iterator it;
-    vector<Strategie> s;
+    vector<Profile> s;
     for (int i = 0; i < nb_strategie1; i++)
     {
         for (int j = 0; j < nb_strategie2; j++)
@@ -316,14 +330,15 @@ void delCol(){
         s.clear();
     }
     int k = 0;
+
     for (auto& c : temp)
     {
-        c = c - k + nb_strategie2;
+        c = c - k + nb_strategie1;
         strategies->erase(strategies->begin()+c);
         nb_strategie2--;
         k++;
     }
-
+    
     matJeu.clear();
     matJeu = v;
     TextTable* t = new TextTable( '-', '|', '+' );
@@ -348,11 +363,11 @@ void delCol(){
     v.clear();
 }
 
-void eissd(){
+void eisfd(){
     while (lineDel || colDel)
     {
-        delLine();
-        delCol();
+        delLine(1);
+        delCol(1);
     }
     if (strategies->size() != 2)
     {
@@ -363,4 +378,205 @@ void eissd(){
         cout << "the equilibria for this game is " << matJeu[0][0].getName() << "=(" << matJeu[0][0].getScorePlayer1() << "," << matJeu[0][0].getscorePlayer2() << ")" << endl;
     }
     lineDel = true;colDel = true;
+}
+
+void eissd(){
+    while (lineDel || colDel)
+    {
+        delLine(2);
+        delCol(2);
+    }
+    if (strategies->size() != 2)
+    {
+        cout << "There is no equilibria for this game!" << endl;
+    }
+    else
+    {
+        cout << "the equilibria for this game is " << matJeu[0][0].getName() << "=(" << matJeu[0][0].getScorePlayer1() << "," << matJeu[0][0].getscorePlayer2() << ")" << endl;
+    }
+    lineDel = true;colDel = true;
+}
+
+void nashEq(){
+    vector<Profile> temp1;
+    vector<Profile> temp2;
+    int max,posMax,cpt=0;
+
+    for (int j = 0; j < nb_strategie2; j++)
+    {
+        for (int i = 0; i < nb_strategie1; i++)
+        {
+            max = matJeu[i][j].getScorePlayer1();
+            for (int k = 0; k < nb_strategie1; k++)
+            {
+                if (k != i)
+                {
+                    if (matJeu[k][j].getScorePlayer1() >= max)
+                    {
+                        max = matJeu[k][j].getScorePlayer1();
+                    }
+                }
+            }
+        }
+        for (int k = 0; k < nb_strategie1; k++)
+        {
+            if (matJeu[k][j].getScorePlayer1() == max)
+            {
+                temp1.push_back(matJeu[k][j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < nb_strategie1; i++)
+    {
+        for (int j = 0; j < nb_strategie2; j++)
+        {
+            max = matJeu[i][j].getscorePlayer2();
+            for (int k = 0; k < nb_strategie2; k++)
+            {
+                if (k != j)
+                {
+                    if (matJeu[i][k].getscorePlayer2() >= max)
+                    {
+                        max = matJeu[i][k].getscorePlayer2();
+                    }
+                }
+            }
+        }
+        for (int k = 0; k < nb_strategie2; k++)
+        {
+            if (matJeu[i][k].getscorePlayer2() == max)
+            {
+                temp2.push_back(matJeu[i][k]);               
+            }
+        }
+    }
+    cout << "Best 1 :";
+    for (auto& c : temp1)
+    {
+        cout << c.getName();
+    }
+    cout << "\nBest 2 :";
+    for (auto& c : temp2)
+    {
+        cout << c.getName();
+    }
+
+    for (auto& c : temp1)
+    {
+        for (auto& c2 : temp2)
+        {
+            if (c.getName() == c2.getName())
+            {
+                cpt++;
+                if (cpt == 1)
+                {
+                    cout << "\nNash equilibrium of this game : ";
+                }
+                cout << c.getName() << " ";
+            }
+        }
+    }
+    if (cpt == 0)
+    {
+        cout << "\nThere is no Nash equilibrium in this game in pure\n";
+    }
+}
+
+int securityLevelStrategy(int player,int s){
+    int min;
+    if (player == 1)
+    {
+        min = matJeu[s][0].getScorePlayer1();
+        for (int j = 1; j < nb_strategie2; j++)
+        {
+            if (matJeu[s][j].getScorePlayer1() <= min)
+            {
+                min = matJeu[s][j].getScorePlayer1();
+            }
+        }
+        return min;
+    }
+    else
+    {
+        min = matJeu[0][s].getscorePlayer2();
+        for (int i = 1; i < nb_strategie1; i++)
+        {
+            if (matJeu[i][s].getscorePlayer2() <= min)
+            {
+                min = matJeu[i][s].getscorePlayer2();
+            }
+        }
+        return min;
+    }
+    
+}
+
+int securityLevelPlayer(int player){
+    int max;
+    if (player == 1)
+    {
+        max = securityLevelStrategy(1,0);
+        for (int i = 1; i < nb_strategie1; i++)
+        {
+            if (securityLevelStrategy(1,i) >= max)
+            {
+                max = securityLevelStrategy(1,i);
+            }
+        }
+        return max;
+    }
+    else
+    {
+        max = securityLevelStrategy(2,0);
+        for (int j = 1; j < nb_strategie2; j++)
+        {
+            if (securityLevelStrategy(2,j) >= max)
+            {
+                max = securityLevelStrategy(2,j);
+            }
+        }
+        return max;
+    }
+    
+}
+
+void pareto(){
+    vector<Profile> finale;
+    bool b = true;
+    for (int i = 0; i < nb_strategie1; i++)
+    {
+        for (int j = 0; j < nb_strategie2; j++)
+        {
+            b = true;
+            for (int k = 0; k < nb_strategie1; k++)
+            {
+                for (int l = 0; l < nb_strategie2; l++)
+                {
+                    if (k != i || l != j)
+                    {
+                        int gain11 = matJeu[i][j].getScorePlayer1(),gain12 = matJeu[i][j].getscorePlayer2(),gain21 = matJeu[k][l].getScorePlayer1(),gain22 = matJeu[k][l].getscorePlayer2();
+                        if ( ((gain21 > gain11 && gain22 >= gain12) || (gain21 >= gain11 && gain22 > gain12)) )
+                        {
+                            b = false;
+                            break;
+                        }
+                    }  
+                }
+                if (!b)
+                {
+                    break;
+                }
+            }
+            if (b)
+            {
+                finale.push_back(matJeu[i][j]);
+            }
+        }
+    }
+    cout << "Profiles that are Optimum pareto are : ";
+    for (auto& c : finale)
+    {
+        cout << c.getName() << " " ;
+    }
 }
--- example.cpp
+++ example.cpp
@@ -4,7 +4,7 @@ int main()
 {   
     system("cls");
     TextTable* t = new TextTable( '-', '|', '+' );
-    vector<vector<Strategie>> payOff;
+    vector<vector<Profile>> payOff;
 
     cout << "How many strategies player 1 have ?" << endl << "==>" ;
     cin >> nb_strategie1;
@@ -15,7 +15,7 @@ int main()
     int nb1 = nb_strategie1,nb2 = nb_strategie2;
     string name;
     
-    vector<Strategie> st;
+    vector<Profile> st;
 
     for (int i = 0; i < nb_strategie1; i++)
     {    
@@ -52,7 +52,7 @@ int main()
             string name = "("+strategies->at(i)+","+strategies->at(j)+")";
             string temp = "("+to_string(scorePlayer1)+","+to_string(scorePlayer2)+")";
             t->add(temp);
-            Strategie* s = new Strategie(name,scorePlayer1,scorePlayer2); 
+            Profile* s = new Profile(name,scorePlayer1,scorePlayer2); 
             st.push_back(*s);
             system("cls");
         }
@@ -68,8 +68,14 @@ int main()
     int choice;
     cout << "\n\nMain Menu:" << endl;
     cout << "==========" << endl;
-    cout << "1 - To determine any strict/weak dominant strategy in the entered game." << endl;
-    cout << "2 - To determine if there is an equilibria in the entered game." << endl; 
+    cout << "1 - To determine any strict dominant strategy in the entered game." << endl;
+    cout << "2 - To determine any weak dominant strategy in the entered game." << endl;
+    cout << "3 - To determine if there is an equilibria in the entered game (EISSD)." << endl;
+    cout << "4 - To determine if there is an equilibria in the entered game (EISFD)." << endl; 
+    cout << "5 - To determine if there is a Nash equilibria in the entered game." << endl;
+    cout << "6 - To determine if there is Pareto profiles." << endl;
+    cout << "7 - To determine security level of each player" << endl;
+    cout << "8 - To determine security level of each strategy" << endl;
     cout << "0 - To exit." << endl;
     cout <<"==>";
     cin >> choice;
@@ -91,16 +97,7 @@ int main()
             }
             else
             {
-                int fdm = determineMDF(1);
-                if (fdm != -1)
-                {
-                    cout << "The weak dominant strategy for player 1 is : ";
-                    cout << strategies->at(fdm) << endl;
-                }
-                else
-                {
-                    cout << "There is no dominant strategy for player 1!" << endl;
-                }
+                cout << "There is no strict dominant strategy for player 1!" << endl;
             }
             if (sdm2 != -1)
             {
@@ -109,19 +106,34 @@ int main()
             }
             else
             {
-                int fdm2 = determineMDF(2);
-                if (fdm2 != -1)
-                {
-                    cout << "The weak dominant strategy for player 2 is : ";
-                    cout << strategies->at(fdm2+nb_strategie1) << endl;
-                }
-                else
-                {
-                    cout << "There is no dominant strategy for player 2!" << endl;
-                }
+                cout << "There is no strict dominant strategy for player 2!" << endl;
             }
         }
         if (choice == 2)
+        {
+            system("cls");
+            int fdm = determineMDF(1);
+            int fdm2 = determineMDF(2);
+            if (fdm != -1)
+            {
+                cout << "The weak dominant strategy for player 1 is : ";
+                cout << strategies->at(fdm) << endl;
+            }
+            else
+            {
+                cout << "There is no weak dominant strategy for player 1!" << endl;
+            }
+            if (fdm2 != -1)
+            {
+                cout << "The weak dominant strategy for player 2 is : ";
+                cout << strategies->at(fdm2+nb_strategie1) << endl;
+            }
+            else
+            {
+                cout << "There is no weak dominant strategy for player 2!" << endl;
+            }
+        }
+        if (choice == 3)
         {
             system("cls");
             eissd();
@@ -131,6 +143,49 @@ int main()
             matJeu.clear();
             matJeu = payOff;
         }
+        if (choice == 4)
+        {
+            system("cls");
+            eisfd();
+            nb_strategie1 = nb1;
+            nb_strategie2 = nb2;
+            *strategies = init;
+            matJeu.clear();
+            matJeu = payOff;
+        }
+        if (choice == 5)
+        {
+            system("cls");
+            nashEq();
+        }
+        if (choice == 6)
+        {
+            system("cls");
+            pareto();
+        }
+        
+        if (choice == 7)
+        {
+            system("cls");
+            cout << "Securtiy level of player 1 :" << securityLevelPlayer(1) << endl;
+            cout << "Securtiy level of player 2 :" << securityLevelPlayer(2) << endl;
+        }
+        if (choice == 8)
+        {
+            system("cls");
+            cout << "Securtiy level of the each strategy of player 1 :" << endl;
+            for (int i = 0; i < nb_strategie1; i++)
+            {
+                cout << strategies->at(i) << ":" << securityLevelStrategy(1,i) << endl;
+            }
+            cout << "Securtiy level of the each strategy of player 2 :" << endl;
+            for (int j = nb_strategie1; j < strategies->size(); j++)
+            {
+                cout << strategies->at(j) << ":" << securityLevelStrategy(2,j-nb_strategie1) << endl;
+            }
+            
+        }
+        
         cout << "\n\nOPTIONS:" << endl;
         cout << "========" << endl;
         cout << "1 - To go back to main menu" << endl; 
